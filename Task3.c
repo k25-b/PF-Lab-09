9cab096a19408075cdb1db38c8e5d24b50e98ba6
k25-b <k256572@nu.edu.pk>
2026-04-06 19:36:34 +0500

    Create Task3.c

---
+++ Task3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int totalRuns(int score_array[], int n) {
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+        total = total + score_array[i];
+    }
+    return total;
+}
+int highestScore(int score_array[], int n) {
+    int highestScore = 0;
+    for (int i = 0; i < n; i++) {
+        if (score_array[i] > highestScore) {
+            highestScore = score_array[i];
+        }
+    }
+    return highestScore;
+}
+int aboveAverage(int score_array[], int n) {
+    int average = 0, count = 0;
+    for (int i = 0; i < n; i++) {
+        average += score_array[i];
+    }
+    average /= n;
+    for (int i = 0; i < n; i++) {
+        if (score_array[i] > average) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int score_array[] = {1, 2, 31, 4, 5, 6, 7, 8, 9, 11};
+    printf("    HIGHEST SCORE: %10d\n", highestScore(score_array, 10));
+    printf("    ABOVE AVG SCORE COUNT: %d\n", aboveAverage(score_array, 10));
+    printf("    TOTAL RUNS: %13d", totalRuns(score_array, 10));
+}
+
+
+
